2c718103fa5b0fc6a85384825c7ecbedad9feed0
AliVik <alvikul1994@gmail.com>
2021-11-05 20:32:10 +0200

    task-01-gets links of button, hang on events listeners, change bodycolor by click

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,16 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const body = document.body;
+console.log(body)
+
+startButton.addEventListener('click', onStartClick);
+// stopButton.addEventListener('click', onStopClick);
+
+
+function onStartClick() {
+    body.style.backgroundColor = getRandomHexColor();
+}
