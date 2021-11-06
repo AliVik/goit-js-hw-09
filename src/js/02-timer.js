0bb0354d292b9991372f345616867bd392b5f297
AliVik <alvikul1994@gmail.com>
2021-11-06 16:28:42 +0200

    добавляет вспомогательные функции,  вывод таймера в консоль

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,26 +3,72 @@ import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const buttonRef = document.querySelector('button[data-start]')
-console.log(buttonRef);
 const inputRef = document.querySelector('input#datetime-picker');
-const currentDay = Date.now();
-console.log('currentDay', currentDay)
+const daysRef = document.querySelector('[data-days]');
+const hoursRef = document.querySelector('[data-hours]');
+const minutesRef = document.querySelector('[data-minutes]');
+const secondsRef = document.querySelector('[data-seconds]');
+const startTime = Date.now();
+
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-      console.log('selectedDay', selectedDates[0].getTime());
-      if (selectedDates[0].getTime() <= currentDay) {
+      console.log('selectedDay: ', selectedDates[0]);
+      
+      if (selectedDates[0].getTime() > startTime) {
+          buttonRef.removeAttribute('disabled');
+          buttonRef.addEventListener('click', () => {
+              setInterval(() => {
+                  const currentTime = Date.now();
+                  const timeDiffer = selectedDates[0].getTime() - currentTime;
+                  const timeToSelectedDate = convertMs(timeDiffer);
+                  console.log(timeToSelectedDate);
+                    
+              }, 1000)
+             
+          });
+      } else {
           Notify.failure('Please choose a date in the future');
           buttonRef.setAttribute('disabled', 'disabled');
-      } else {
-          buttonRef.removeAttribute('disabled');
       }
-      
-      
+     
   },
 };
 
+buttonRef.setAttribute('disabled', 'disabled');
 flatpickr(inputRef, options);
+
+function updateClockInterface({days,hours,minutes,seconds}) {
+    daysRef.value = `${days}`;
+    hoursRef.value = `${hours}`;
+    minutesRef.value = `${minutes}`;
+    secondsRef.value = `${seconds}`;
+
+}
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
